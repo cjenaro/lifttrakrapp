f7a2d5898dc3f4550b3c1b9ef0d0d0be3cf70151
Jenaro Calvino <jen.calvineo@gmail.com>
2021-03-24 15:03:21 -0300

    fix(useweight): added retry false

--- src/hooks/use-weight.ts
+++ src/hooks/use-weight.ts
@@ -7,17 +7,20 @@ function calculateWeight(raw: string, reps: string) {
 
 export default function useWeight(exercise: string, reps: string) {
   const { user } = useAuth();
-  const { data, error, isLoading } = useQuery(["weight", exercise], () =>
-    fetch("/.netlify/functions/get-weight", {
-      method: "POST",
-      body: JSON.stringify({
-        exercise,
-        user: user?.id,
-      }),
-      headers: {
-        authorization: `Bearer ${user?.token}`,
-      },
-    }).then((blob) => blob.json())
+  const { data, error, isLoading } = useQuery(
+    ["weight", exercise],
+    () =>
+      fetch("/.netlify/functions/get-weight", {
+        method: "POST",
+        body: JSON.stringify({
+          exercise,
+          user: user?.id,
+        }),
+        headers: {
+          authorization: `Bearer ${user?.token}`,
+        },
+      }).then((blob) => blob.json()),
+    { retry: false }
   );
 
   return {
