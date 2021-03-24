58c14ea90e4bf78cd67443d7bd8a13c7dcd36eec
Jenaro Calvino <jen.calvineo@gmail.com>
2021-03-24 16:17:37 -0300

    fix(home): added overflow hidden

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,7 @@ import { Link as RLink, RouteComponentProps } from "@reach/router";
 
 export default function Home(props: RouteComponentProps) {
   return (
-    <SimpleGrid columns={1} height="100%" rowGap={6}>
+    <SimpleGrid columns={1} height="100%" rowGap={6} overflow="hidden">
       <Button
         height="100%"
         textTransform="uppercase"
